bb9e2699a14b6c11dbf95f6f0ce3e5bdc8befa46
Thanasit008 <tanasitjin@gmail.com>
2026-08-26 14:24:50 +0700

    Fix: Add missing handleSetSpeed function implementation

--- esp32/fish_feeder/fish_feeder.ino
+++ esp32/fish_feeder/fish_feeder.ino
@@ -1118,3 +1118,16 @@ void testMotorSerial() {
     }
   }
 }
+void handleSetSpeed() {
+  if (server.hasArg("speed")) {
+    currentMotorSpeed = server.arg("speed").toInt();
+    if(currentMotorSpeed < 0) currentMotorSpeed = 0;
+    if(currentMotorSpeed > 100) currentMotorSpeed = 100;
+    preferences.begin("motor_config", false);
+    preferences.putInt("speed", currentMotorSpeed);
+    preferences.end();
+    server.send(200, "application/json", "{\"success\":true,\"message\":\"Speed updated\"}");
+  } else {
+    server.send(400, "application/json", "{\"success\":false,\"message\":\"Missing speed\"}");
+  }
+}
